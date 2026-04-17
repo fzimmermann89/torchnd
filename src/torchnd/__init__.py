d3af7822c4a30186685f4f103cfeb8c67d2c9bc7
Felix F Zimmermann <fzimmermann89@gmail.com>
2026-04-17 16:38:48 +0200

    Bump version

--- src/torchnd/__init__.py
+++ src/torchnd/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from torchnd.functional import adjoint_pad_nd, conv_nd, pad_nd, pad_or_crop_to_size
 from torchnd.modules import ConvNd, ConvTransposeNd
